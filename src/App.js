7e0cd6f57bc01bd41b81af1f63e25a22519575ac
Caroline <carolinegrabowski@Carolines-MacBook-Pro.local>
2019-12-03 16:35:44 -0500

    quiz attempt / added routing

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import React from 'react';
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import HomePage from './components/HomePage'
+import NumberButton from './components/NumberButton'
+
 
 function App() {
+  const [showComp, setShowComp] = useState(true);
+  // let toggleComponents = true;
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={e => setShowComp(!showComp)}>Toggle</button>
+      { showComp ? <HomePage/> : <NumberButton/> }
     </div>
   );
 }
---
+++ src/components/HomePage.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function HomePage() {
+    return (
+        <div className="homePage">
+            <header className="header">
+                <h1>H O M E P A G E</h1>
+            </header>
+            <body className="body">
+                <b>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</b>
+            </body>
+        </div>
+    )   
+}
+
+export default HomePage;
